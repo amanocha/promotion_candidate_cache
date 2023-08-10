a26d024137ed94225e16936b8c46e099c1bcd59f
Aninda Manocha <anindamanocha@Anindas-MacBook-Pro.local>
2023-08-10 16:10:11 -0700

    refactoring

--- applications/measure.py
+++ applications/measure.py
@@ -6,19 +6,11 @@ def parse_args():
     parser.add_argument("-s", "--source", type=str, help="Path to source code file")
     parser.add_argument("-d", "--data", type=str, help="Path to data file")
     parser.add_argument("-rp", "--relative_path", type=int, default=0, help="Use relative data file path")
-    parser.add_argument("-e", "--epoch", type=int, default=-1, help="Epoch used for sampling")
-    parser.add_argument("-n", "--compute_edges", type=int, default=-1, help="Number of compute edges to sample with")
     parser.add_argument("-ss", "--start_seed", type=int, default=-1, help="Start seed for BFS and SSSP")
     parser.add_argument("-t", "--num_threads", type=int, default=1, help="Number of threads")
-    parser.add_argument("-i", "--thread_id", type=int, default=0, help="Thread ID")
-    parser.add_argument("-m", "--mode", type=str, default="db", help="Compilation variant: decades_base (db) or decades_decoupled_implicit (di) or perfect cache (PC)")
     parser.add_argument("-o", "--output", type=str, help="Output path")
     parser.add_argument("-mo", "--measure_only", type=int, default=0, help="Perform parsing of results only (no compilation or application execution)")
-    parser.add_argument("-ma", "--madvise", type=str, default="1", help="Madvise mode")
-    parser.add_argument("-ml", "--mlock", type=str, default="0", help="Mlock mode")
-    parser.add_argument("-nd", "--num_demotions", type=str, default="0", help="Number of demotion scans")
-    parser.add_argument("-hk", "--hawkeye_percentage", type=str, default="0", help="Percentage of program to consider HawkEye to guide promotions")
-    parser.add_argument("-rd", "--reuse_dist", type=str, default="0", help="Use reuse distance to guide promotions")
+    parser.add_argument("-ma", "--madvise", type=str, default="0", help="Madvise mode")
     parser.add_argument("-pd", "--promotion_data", type=str, default="", help="Promotion filename")
     parser.add_argument("-dd", "--demotion_data", type=str, default="", help="Demotion filename")
     parser.add_argument("-mid", "--multiprocess_id", type=int, default=0, help="Multiprocess ID")
@@ -37,7 +29,7 @@ def compile():
     print(cmd)
     os.system(cmd)
 
-def execute(run_kernel, num_demotions="0", mlock="0", iteration=""):
+def execute(run_kernel, iteration=""):
     print("Executing application...")
     if (relative):
         input_path = os.path.relpath(data, new_dir)
@@ -73,7 +65,6 @@ def execute(run_kernel, num_demotions="0", mlock="0", iteration=""):
 
     cmd_args += [perf_cmd]
 
-    thp_name = "thp_output_" + run_kernel + "_" + iteration + ".txt" if iteration != "" else "thp_output_" + run_kernel + ".txt"
     access_name = "access_output_" + run_kernel + "_" + iteration + ".txt" if iteration != "" else "access_output_" + run_kernel + ".txt"
     pf_name = "pf_output_" + run_kernel + "_" + iteration + ".txt" if iteration != "" else "pf_output_" + run_kernel + ".txt"
     cmd_args += [access_name, pf_name]
@@ -83,15 +74,6 @@ def execute(run_kernel, num_demotions="0", mlock="0", iteration=""):
 
     if demotion_data != "":
         cmd_args += [demotion_data]
-
-    if num_demotions != "0":
-        cmd_args += [num_demotions]
-
-    if mlock != "0":
-        cmd_args += ["1"]
-
-    #if (threads > 1):
-    #    cmd_args += [global_wait_output + "done" + str(tid) + ".txt", global_wait_output, str(threads)]
 
     cmd_args += [">", app_out_name]
     cmd_args += ["2>", err_name]
@@ -156,93 +138,6 @@ def avg(N):
     for metric in metric_vals:
       metric_vals[metric] = round(float(metric_vals[metric])/N, 2)
       measurements.write(metric + ": " + str(metric_vals[metric]) + "\n")
-
-    measurements.close()
-
-def test_parse_results(run_kernel, iteration=""):
-    if iteration != "":
-      results_file = "results_" + iteration + ".txt"
-      measurements_file = "measurements_" + iteration + ".txt"
-    else:
-      results_file = "results.txt"
-      measurements_file = "measurements.txt"
-
-    measurements = open(output + results_file, "w+")
-
-    if (measure_only):
-      measure = False
-      if os.path.isfile(output + measurements_file):
-        averages = open(output + measurements_file, "r+")
-        measure = True
-      elif os.path.isfile(output + "perf_output_" + run_kernel + ".txt"):
-        averages = open(output + "perf_output_" + run_kernel + ".txt", "r+")
-      else:
-        return
-      for line in averages:
-        line = line.replace(",","").replace("-", "")
-        if measure:
-          match = re.match("\s*(\w+\.?\w*\.?\w*\.?\w*\.?\w*):\s+(\d+\.*\d*e*\+*\d*)", line)
-          if (match != None):
-            metric = match.group(1)
-            value = float(match.group(2))
-            if metric in metric_vals:
-              metric_vals[metric] += value
-            else:
-              metric_vals[metric] = value
-            print(metric, value)
-        else:
-          match = re.match("\s*(\d+)\s+(\w+\.?\w*\.?\w*\.?\w*\.?\w*)", line)
-          if (match != None):
-            metric = match.group(2)
-            value = int(match.group(1))
-            if metric in metric_vals:
-              metric_vals[metric] += value
-            else:
-              metric_vals[metric] = value
-            print(metric, value)
-      averages.close()
-
-    measurements.write("RESULTS:\n")
-    measurements.write("----------\n")
-    measurements.write("\n")
-
-    cycles = metric_vals["cpucycles"]
-
-    #tlb_ld_misses = metric_vals["dTLBloadmisses"]
-    #tlb_st_misses = metric_vals["dTLBstoremisses"]
-    tlb_lds = metric_vals["dTLBloads"]
-    tlb_sts = metric_vals["dTLBstores"]
-    if tlb_sts < 0:
-      tlb_sts = 0
-    tlb_refs = tlb_lds + tlb_sts
-
-    tlb_ld_stlb = metric_vals["dtlb_load_misses.stlb_hit"]
-    tlb_ld_walks = metric_vals["dtlb_load_misses.miss_causes_a_walk"]
-    tlb_ld_walk_cycles = metric_vals["dtlb_load_misses.walk_duration"]
-    tlb_ld_walk_completed = metric_vals["dtlb_load_misses.walk_completed"]
-    tlb_st_stlb = metric_vals["dtlb_store_misses.stlb_hit"]
-    tlb_st_walks = metric_vals["dtlb_store_misses.miss_causes_a_walk"]
-    tlb_st_walk_cycles = metric_vals["dtlb_store_misses.walk_duration"]
-    tlb_st_walk_completed = metric_vals["dtlb_store_misses.walk_completed"]
-    page_faults = metric_vals["pagefaults"]
-    tlb_stlb = tlb_ld_stlb + tlb_st_stlb
-    tlb_walks = tlb_ld_walks + tlb_st_walks
-    tlb_misses = tlb_stlb + tlb_walks
-    tlb_walk_cycles = tlb_ld_walk_cycles + tlb_st_walk_cycles
-    tlb_walk_completed = tlb_ld_walk_completed + tlb_st_walk_completed
-    tlb_miss_rate = tlb_misses*100.0/tlb_refs if (tlb_refs > 0) else 0
-
-    measurements.write("TLB:\n")
-    measurements.write("TLB Miss Rate: " + str(tlb_miss_rate) + "\n")
-    if (tlb_walks > 0):
-      measurements.write("STLB Miss Rate: " + str(tlb_walks*100.0/(tlb_walks+tlb_stlb)) + "\n")
-      measurements.write("Page Fault Rate: " + str(page_faults*100.0/tlb_walks) + "\n")
-    if (tlb_refs > 0):
-      measurements.write("Percent of TLB Accesses with PT Walks: " + str(tlb_walks*100.0/tlb_refs) + "\n")
-      measurements.write("Percent of TLB Accesses with Completed PT Walks: " + str(tlb_walk_completed*100.0/tlb_refs) + "\n")
-      measurements.write("Percent of TLB Accesses with Page Faults: " + str(page_faults*100.0/tlb_refs) + "\n")
-    measurements.write("Percent of Cycles Spent on PT Walks: " + str(tlb_walk_cycles*100.0/cycles) + "\n")
-    measurements.write("Average Cycles Spent on PT Walk: " + str(tlb_walk_cycles*100.0/tlb_walk_completed) + "\n")
 
     measurements.close()
 
@@ -423,10 +318,8 @@ def parse_results(run_kernel, iteration=""):
     measurements.close()
 
 def main():
-    global source, data, relative, measure_only, output, metrics
-    global filename, mode, compile_dir, flags, threads, tid
-    global app_name, app_input, new_dir, epoch, compute_edges, start_seed, global_wait_output, multiprocess_id
-    global hawkeye_percentage, reuse_dist, promotion_data, demotion_data
+    global source, data, relative, measure_only, output, filename, threads
+    global app_name, app_input, new_dir, start_seed, multiprocess_id, promotion_data, demotion_data
 
     args = parse_args()
 
@@ -437,23 +330,10 @@ def main():
         data = args.data
         relative = args.relative_path
 
-        #metrics += vm_cafe
-        #metrics += pipeline_cafe
-
         filename = os.path.basename(source)
-        mode = args.mode
         threads = args.num_threads
-        tid = args.thread_id
-        hawkeye_percentage = args.hawkeye_percentage
-        reuse_dist = args.reuse_dist
         promotion_data = args.promotion_data
         demotion_data = args.demotion_data
-
-        if mode == "di":
-            compile_dir = "decades_decoupled_implicit"
-            flags = flags + "-d"
-        else:
-            compile_dir = "decades_base"
 
         app_name = source.split("/")[-2]
         if (app_name.replace("_multiprocess", "") not in vp):
@@ -462,8 +342,6 @@ def main():
           names = data.split('/')
           app_input = names[len(names)-2] 
         new_dir = os.path.dirname(source)
-        epoch = args.epoch
-        compute_edges = args.compute_edges
         start_seed = args.start_seed
 
         print("Application: " + app_name)
@@ -474,7 +352,7 @@ def main():
         if args.output:
           output = args.output
         else:
-          output = "/home/amanocha/graph_apps/results/" + app_name + "_" + app_input + "/"
+          output = HOME_DIR + "results/" + app_name + "_" + app_input + "/"
 
         if (not os.path.isdir(output)):
           os.mkdir(output)
@@ -486,8 +364,6 @@ def main():
 
         num_samples = args.num_samples
         run_kernel = args.madvise
-        num_demotions = args.num_demotions
-        mlock = args.mlock
 
         if (measure_only):
           parse_results(run_kernel)
@@ -495,7 +371,7 @@ def main():
           if "multiprocess" not in source:
             compile()
           for i in range(num_samples):
-            execute(run_kernel, num_demotions, mlock, str(i))
+            execute(run_kernel, str(i))
             measure(run_kernel, str(i))
             parse_results(run_kernel, str(i))
           avg(num_samples)
