9528649976aa475404d530f6cb23a8f307f3d925
Aninda Manocha <anindamanocha@Anindas-MacBook-Pro.local>
2023-08-10 16:10:11 -0700

    refactoring

--- applications/go.py
+++ applications/go.py
@@ -8,17 +8,15 @@ from subprocess import Popen, PIPE
 
 HOME_DIR = "/home/aninda/promotion_candidate_cache/"
 LAUNCH_DIR = HOME_DIR + "applications/launch/" 
-MAIN_DIR = HOME_DIR + "results/"
+RESULT_DIR = HOME_DIR + "results/"
 GRAPH_DIR = HOME_DIR + "data/"
 PROMOTION_CACHE_DIR = HOME_DIR + "pin3.7/source/tools/PromotionCache/output/promotion_data/"
 DEMOTION_CACHE_DIR = HOME_DIR + "pin3.7/source/tools/PromotionCache/output/demotion_data/"
 
 KB_PER_THP = 2048
-CACHE_SIZE = 128
+PCC_SIZE = 128
 ACCESS_TIME = 30
 
-REUSE_INTERVAL = 512
-PROMOTION_FIXED = 21
 HAWKEYE = 22
 MADVISE_ALL = 50
 
@@ -63,27 +61,6 @@ dataset_names = {"DBG_Kronecker_25/": "dbg_kron25",
             "mcf_speed_inp.in": "mcf",
             "omnetpp.ini": "omnetpp",
             "t5.xml": "xalancbmk"}
-plot_names = {"DBG_Kronecker_25/": "Sorted Kronecker 25",
-            "DBG_Twitter/": "Sorted Twitter",
-            "DBG_Sd1_Arc/": "Sorted Sd1 Arc Web",
-            "DBG_Wikipedia/": "Sorted Wikipedia",
-            "Kronecker_25/": "Kronecker 25",
-            "Twitter/": "Twitter",
-            "Sd1_Arc/": "Sd1 Arc Web",
-            "Wikipedia/": "Wikipedia",
-            "canneal_native.in": "canneal",
-            "dedup_native.in": "dedup",
-            "mcf_speed_inp.in": "mcf",
-            "omnetpp.ini": "omnetpp",
-            "t5.xml": "xalancbmk"}
-app_names = {"bfs": "BFS",
-             "sssp": "SSSP",
-             "pagerank": "PR",
-             "canneal": "canneal",
-             "dedup": "dedup",
-             "mcf": "mcf",
-             "omnetpp": "omnetpp",
-             "xalancbmk": "xalancbmk"}
 
 vp_inputs = []
 for dataset in datasets:
@@ -133,7 +110,6 @@ def run(cmd, tmp_output, output):
 
 # Experiment 1
 def run_one(madvise):
-  global GRAPH_DIR
   if madvise == 0:
     exp_name = "thp" if is_thp == 1 else "none"
   elif madvise == MADVISE_ALL:
@@ -166,9 +142,7 @@ def run_one(madvise):
         run(cmd, tmp_output, exp_dir + output)
 
 # Experiment 2: HawkEye or Promotion Cache    
-def run_two(exp_type, config, size=512, access_time=30, num_threads=1, policy=0, demotion=False, promote_all=False):
-  global GRAPH_DIR
-  
+def run_two(exp_type, config, size=512, access_time=30, num_threads=1, policy=0, demotion=False, promote_all=False): 
   if "cache" in exp_type:
     exp_type += "_" + str(size) 
   exp_name = str(num_threads) + "_threads/" if num_threads > 1 else ""
@@ -235,7 +209,6 @@ def run_two(exp_type, config, size=512, access_time=30, num_threads=1, policy=0,
 
 # Experiment 3: Multiprocess 
 def run_three(exp_type, config, size=512, policy=0):
-  global GRAPH_DIR
   if config == 0:
     exp_name = "thp" if is_thp == 1 else "none"
     madvise = "0"
@@ -281,7 +254,7 @@ def run_three(exp_type, config, size=512, policy=0):
       time.sleep(1)
 
 def main():
-  global apps, is_thp, RESULT_DIR, GRAPH_DIR, inputs, datasets, vp_inputs
+  global apps, is_thp, RESULT_DIR, inputs, datasets, vp_inputs
 
   args = parse_args()
 
@@ -307,7 +280,7 @@ def main():
     apps = [args.app]
 
   if args.experiment == 0:
-    RESULT_DIR = MAIN_DIR + "single_thread/"
+    RESULT_DIR += "single_thread/"
     if not os.path.isdir(RESULT_DIR):
       os.mkdir(RESULT_DIR)
     run_two("cache", 0, 512)
@@ -318,7 +291,7 @@ def main():
     for i in range(0, 21):
       run_one(i)
   elif args.experiment == 2:
-    RESULT_DIR = MAIN_DIR + "single_thread/"
+    RESULT_DIR += "single_thread/"
     if not os.path.isdir(RESULT_DIR):
       os.mkdir(RESULT_DIR)
     run_two("hawkeye", 0, 512)
@@ -327,21 +300,21 @@ def main():
       run_two("hawkeye", percentage, 512)
     run_two("hawkeye", 100, 512)
   elif args.experiment == 3:
-    RESULT_DIR = MAIN_DIR + "single_thread/"
+    RESULT_DIR += + "single_thread/"
     if not os.path.isdir(RESULT_DIR):
       os.mkdir(RESULT_DIR)
     run_two("cache", 0, 512, access_time=ACCESS_TIME)
     for i in range(NUM_PERCENTAGES):
       percentage = int(math.pow(2, i))
-      run_two("cache", percentage, CACHE_SIZE, access_time=ACCESS_TIME, promote_all=True)
-    run_two("cache", 100, CACHE_SIZE, access_time=ACCESS_TIME, promote_all=True)
+      run_two("cache", percentage, PCC_SIZE, access_time=ACCESS_TIME, promote_all=True)
+    run_two("cache", 100, PCC_SIZE, access_time=ACCESS_TIME, promote_all=True)
     for i in range(NUM_SIZES):
       size = int(math.pow(2, i+2))
       for f in range(NUM_PERCENTAGES):
         run_two("cache", int(math.pow(2, f)), size)
       run_two("cache", 100, size)
   elif args.experiment == 4:
-    RESULT_DIR = MAIN_DIR + "multithread/"
+    RESULT_DIR += "multithread/"
     if not os.path.isdir(RESULT_DIR):
       os.mkdir(RESULT_DIR)
     for policy in range(2):
@@ -355,15 +328,15 @@ def main():
         run_two("cache", 100, 512, num_threads, policy)
         run_two("cache", 100, 512, num_threads, policy)
   elif args.experiment == 5:
-    RESULT_DIR = MAIN_DIR + "frag50/"
+    RESULT_DIR += "frag50/"
     if not os.path.isdir(RESULT_DIR):
       os.mkdir(RESULT_DIR)
-    run_two("cache", 0, CACHE_SIZE)
-    run_two("cache", 100, CACHE_SIZE)
-    run_two("hawkeye", 100, CACHE_SIZE)
-    run_two("cache", 100, CACHE_SIZE, demotion=True)
+    run_two("cache", 0, PCC_SIZE)
+    run_two("cache", 100, PCC_SIZE)
+    run_two("hawkeye", 100, PCC_SIZE)
+    run_two("cache", 100, PCC_SIZE, demotion=True)
   elif args.experiment == 6:
-    RESULT_DIR = MAIN_DIR + "multiprocess/"
+    RESULT_DIR += "multiprocess/"
     if not os.path.isdir(RESULT_DIR):
       os.mkdir(RESULT_DIR)
     run_three("cache", 0, 512)
