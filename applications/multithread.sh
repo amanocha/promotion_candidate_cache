fe6b27593aa5277670d1a109b9f9b41ce66f8127
Aninda Manocha <amanocha@princeton.edu>
2023-09-10 10:31:15 -0400

    Update multithread.sh

--- applications/multithread.sh
+++ applications/multithread.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Run simulations for PCC promotion data
-echo "sudo bash ../pin3.7/source/tools/run.sh pcc multithread"
-sudo bash ../pin3.7/source/tools/run.sh pcc multithread
+echo "sudo bash ../pin3.7/source/tools/PromotionCache/run.sh pcc multithread"
+sudo bash ../pin3.7/source/tools/PromotionCache/run.sh pcc multithread
 
 # Huge Page Utility
 echo "sudo python go.py -x multithread"
